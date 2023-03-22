deeb8b9c1c9989dc0282c756552360087da6178f
yoyobojo <angellolazar@gmail.com>
2023-03-22 16:43:36 -0400

    added different button types

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -4,13 +4,22 @@ type IButtonProps = {
     children: ReactNode | string;
     onClick?: MouseEventHandler<HTMLButtonElement>;
     className?: string;
+    type?: 'outline' | 'primary';
 };
 
-export const Button = ({ children, onClick, className }: IButtonProps) => {
+export const Button = ({ children, onClick, className, type }: IButtonProps) => {
+    const renderType = () => {
+        switch (type) {
+            case 'outline':
+                return 'bg-gray-900';
+            default:
+                return 'bg-indigo-800 hover:bg-indigo-900';
+        }
+    };
     return (
         <button
             onClick={onClick}
-            className={`${className} bg-sky-800 px-2.5 py-1.5 lg:px-4 lg:py-2.5 rounded shadow transition duration-150 hover:bg-sky-900`}
+            className={`${className} ${renderType()} px-2.5 py-1.5 lg:px-4 lg:py-2.5 rounded shadow transition duration-150 border-2 border-indigo-800 hover:border-indigo-900`}
         >
             {children}
         </button>
