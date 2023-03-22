615b73266f76cfed03337bd7ce22ba47a3baf93c
yoyobojo <angellolazar@gmail.com>
2023-03-22 11:11:46 -0400

    connect wallet screen complete

--- src/base/navbar.tsx
+++ src/base/navbar.tsx
@@ -4,12 +4,12 @@ import { Wallet } from '../components';
 export const Navbar = () => {
     const { address } = useAccount();
     return (
-        <nav className="py-2 lg:py-4 px-3 lg:px-6 w-full grid grid-cols-3">
-            <div></div>
-            <div className="mx-auto">
+        <nav className="py-2 lg:py-4 px-3 lg:px-6 w-full grid grid-cols-3 items-center">
+            <div className="order-2 lg:order-1"></div>
+            <div className="lg:mx-auto order-1 lg:order-2">
                 <img src="/logo/logo-only.png" alt="PintSwap Logo" height="60" width="60" />
             </div>
-            <div className="justify-self-end">{address && <Wallet />}</div>
+            <div className="justify-self-end order-3">{address && <Wallet />}</div>
         </nav>
     );
 };
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -10,7 +10,7 @@ export const Button = ({ children, onClick, className }: IButtonProps) => {
     return (
         <button
             onClick={onClick}
-            className={`${className} bg-sky-800 px-4 py-2.5 rounded shadow transition duration-150 hover:bg-sky-900`}
+            className={`${className} bg-sky-800 px-2.5 py-1.5 lg:px-4 lg:py-2.5 rounded shadow transition duration-150 hover:bg-sky-900`}
         >
             {children}
         </button>
