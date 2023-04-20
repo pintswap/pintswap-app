f12f7181c31b9c303ccf730cb8f81a3a6456e153
yoyobojo <angellolazar@gmail.com>
2023-04-19 20:09:53 -0400

    added tabs to account page

--- src/views/account.tsx
+++ src/views/account.tsx
@@ -6,12 +6,16 @@ import { useWindowSize } from '../hooks/window-size';
 import { useOffersContext } from '../stores';
 import { useGlobalContext } from '../stores/global';
 import { convertAmount } from '../utils/common';
+import { Tab } from '@headlessui/react';
+import { Fragment } from 'react';
 
 export const AccountView = () => {
     const { width } = useWindowSize();
     const navigate = useNavigate();
     const { pintswap } = useGlobalContext();
     const { openTrades } = useOffersContext();
+
+    const TABS = ['Your Orders', 'Settings']
 
     return (
         <div className="flex flex-col gap-6">
@@ -26,34 +30,57 @@ export const AccountView = () => {
                     />
                 </Skeleton>
             </div>
-            <Card header="Your Trades" scroll>
-                <Table
-                    headers={['Hash', 'Sending', 'Receiving']}
-                    onClick={(order: any) =>
-                        navigate(`/${pintswap?.module?.peerId.toB58String()}/${order.hash}`)
+            <Tab.Group>
+                <Card 
+                    header={
+                        <Tab.List className="grid grid-cols-2 gap-2 lg:gap-4">
+                            {TABS.map((tab, i) => (
+                                <Tab key={`tabs-${i}`}>
+                                    {({ selected }) => (
+                                        <button className={`${selected ? 'border-indigo-600' : 'border-transparent hover:border-neutral-800 hover:text-indigo-200'} border-b-2 lg:border-b-4 rounded w-full pb-2 transition duration-200`}>
+                                            {tab}
+                                        </button>
+                                    )}
+                                </Tab>
+                            ))}
+                        </Tab.List>
                     }
-                    items={Array.from(openTrades, (entry) => ({
-                        hash: entry[0],
-                        gives: convertAmount('readable', (entry[1].gives.amount || ''), entry[1].gives.token),
-                        gets: convertAmount('readable', (entry[1].gets.amount || ''), entry[1].gets.token),
-                    }))}
-                    emptyContent={
-                        pintswap.loading ? (
-                            <ImSpinner9 className="animate-spin" size="20px" />
-                        ) : (
-                            <span>
-                                You currently have no open trades.{' '}
-                                <button onClick={() => navigate('/create')}>
-                                    Create a trade now!
-                                </button>
-                            </span>
-                        )
-                    }
-                />
-            </Card>
+                    scroll
+                >
+                    <Tab.Panels>
+                        <Tab.Panel>
+                            <Table
+                                headers={['Hash', 'Sending', 'Receiving']}
+                                onClick={(order: any) =>
+                                    navigate(`/${pintswap?.module?.peerId.toB58String()}/${order.hash}`)
+                                }
+                                items={Array.from(openTrades, (entry) => ({
+                                    hash: entry[0],
+                                    gives: convertAmount('readable', (entry[1].gives.amount || ''), entry[1].gives.token),
+                                    gets: convertAmount('readable', (entry[1].gets.amount || ''), entry[1].gets.token),
+                                }))}
+                                emptyContent={
+                                    pintswap.loading ? (
+                                        <ImSpinner9 className="animate-spin" size="20px" />
+                                    ) : (
+                                        <span>
+                                            You currently have no open trades.{' '}
+                                            <button onClick={() => navigate('/create')}>
+                                                Create a trade now!
+                                            </button>
+                                        </span>
+                                    )
+                                }
+                            />
+                        </Tab.Panel>
+                        <Tab.Panel>Settings</Tab.Panel>
+                    </Tab.Panels>
+                </Card>
+            </Tab.Group>
             <Button onClick={() => navigate('/create')} className="sm:max-w-lg sm:self-center">
                 Create Order
             </Button>
+            
         </div>
     );
 };
