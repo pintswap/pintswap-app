8106fc783f67c9904c49771befda7a0061b271a0
yoyobojo <angellolazar@gmail.com>
2023-04-27 18:59:51 -0400

    create trade nft view

--- src/views/create.tsx
+++ src/views/create.tsx
@@ -1,4 +1,4 @@
-import { Transition } from '@headlessui/react';
+import { Tab, Transition } from '@headlessui/react';
 import {
     Button,
     Card,
@@ -14,17 +14,19 @@ import { BASE_URL } from '../utils/common';
 export const CreateView = () => {
     const { broadcastTrade, loading, trade, order, updateTrade, steps } = useTrade();
 
+    const TABS = ['ERC20', 'NFT']
     return (
         <>
             <div className="flex flex-col gap-6">
                 <div className="flex flex-col">
                     <h2 className="view-header">Create Trade</h2>
-                    <Card className="self-center">
+                    <Card type="tabs" tabs={TABS}>
+                        <Tab.Panel>
                         <div className="grid grid-cols-1 gap-6 lg:gap-y-2 items-start">
                             <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-3 items-start">
                                 <DropdownInput
                                     title="Send Details"
-                                    placeholder="Select a token..."
+                                    placeholder="Select a token"
                                     state={trade.gives.token}
                                     setState={updateTrade}
                                     type="gives.token"
@@ -47,7 +49,7 @@ export const CreateView = () => {
                             <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-3 items-start">
                                 <DropdownInput
                                     title="Receive Details"
-                                    placeholder="Select a token..."
+                                    placeholder="Select a token"
                                     state={trade.gets.token}
                                     setState={updateTrade}
                                     type="gets.token"
@@ -68,6 +70,57 @@ export const CreateView = () => {
                                 />
                             </div>
                         </div>
+                        </Tab.Panel>
+                        <Tab.Panel>
+                        <div className="grid grid-cols-1 gap-6 items-start">
+                            <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-3 items-start">
+                                <Input
+                                    title="Send Details"
+                                    placeholder="NFT Address"
+                                    value={trade.gives.token || ''}
+                                    onChange={({ currentTarget }) =>
+                                        updateTrade('gives.token', currentTarget.value)
+                                    }
+                                    type="text"
+                                    token={trade.gives.token || true}
+                                    disabled={!!order.orderHash}
+                                />
+                                <Input
+                                    placeholder="NFT Token ID"
+                                    value={trade.gives.tokenId || ''}
+                                    onChange={({ currentTarget }) =>
+                                        updateTrade('gives.amount', currentTarget.value)
+                                    }
+                                    type="number"
+                                    token={trade.gives.tokenId || true}
+                                    disabled={!!order.orderHash}
+                                />
+                            </div>
+                            <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-3 items-start">
+                                <DropdownInput
+                                    title="Receive Details"
+                                    placeholder="Select a token"
+                                    state={trade.gets.token}
+                                    setState={updateTrade}
+                                    type="gets.token"
+                                    search
+                                    disabled={!!order.orderHash}
+                                    customInput
+                                />
+                                <Input
+                                    placeholder="Amount to Receive"
+                                    value={(trade.gets.amount || '')}
+                                    onChange={({ currentTarget }) =>
+                                        updateTrade('gets.amount', currentTarget.value.toUpperCase())
+                                    }
+                                    type="number"
+                                    token={trade.gets.token || true}
+                                    maxClick={updateTrade}
+                                    disabled={!!order.orderHash}
+                                />
+                            </div>
+                        </div>
+                        </Tab.Panel>
                         <Button
                             checkNetwork
                             className="mt-6 w-full"
