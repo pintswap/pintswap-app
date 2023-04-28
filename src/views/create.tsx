443cfd98f5fd30a4d042025b5063694cb8896795
yoyobojo <angellolazar@gmail.com>
2023-04-28 12:41:17 -0400

    pushin to develop

--- src/views/create.tsx
+++ src/views/create.tsx
@@ -1,4 +1,5 @@
 import { Tab, Transition } from '@headlessui/react';
+import { useEffect, useState } from 'react';
 import {
     Button,
     Card,
@@ -7,10 +8,12 @@ import {
     DropdownInput,
     PageStatus,
     DataTable,
+    NFTDisplay,
 } from '../components';
 import { useTrade } from '../hooks/trade';
 import { useOffersContext } from '../stores';
-import { BASE_URL, convertAmount } from '../utils/common';
+import { BASE_URL, convertAmount, INFTProps } from '../utils/common';
+import { fetchNFT } from '../utils/fetch-nft';
 
 const columns = [
     {
@@ -30,6 +33,7 @@ const columns = [
 export const CreateView = () => {
     const { broadcastTrade, loading, trade, order, updateTrade, steps } = useTrade();
     const { openTrades } = useOffersContext();
+    const [nft, setNFT] = useState<INFTProps | null>(null);
 
     const createTradeLink = () => {
         let finalUrl = `${BASE_URL}/#/fulfill/${order.multiAddr}`;
@@ -41,12 +45,42 @@ export const CreateView = () => {
         return finalUrl;
     }
 
+    useEffect(() => {
+        (async () => {
+            const { gives } = trade;
+            if (gives.tokenId && gives.token) {
+                const n = await fetchNFT({ token: gives.token, tokenId: gives.tokenId });
+                setNFT(n);
+            }
+        })().catch((err) => console.error(err));
+    }, [trade.gives.tokenId, trade.gives.token]);
+
     const TABS = ['ERC20', 'NFT']
     return (
         <>
             <div className="flex flex-col gap-6">
                 <div className="flex flex-col">
-                    <h2 className="view-header">Create Trade</h2>
+                    <div className="flex items-center justify-between mb-6">
+                        <h2 className="view-header">Create Trade</h2>
+                        <Transition
+                            show={!!order.orderHash && !!order.multiAddr}
+                            enter="transition-opacity duration-75"
+                            enterFrom="opacity-0"
+                            enterTo="opacity-100"
+                            leave="transition-opacity duration-150"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                            className="flex flex-col justify-center items-start text-center"
+                        >
+                            <p className="text-sm">Trade Link:</p>
+                            <CopyClipboard
+                                value={createTradeLink()}
+                                icon
+                                lg
+                                truncate={5}
+                            />
+                        </Transition>
+                    </div>
                     <Card type="tabs" tabs={TABS}>
                         <Tab.Panel>
                         <div className="grid grid-cols-1 gap-6 lg:gap-y-2 items-start">
@@ -100,6 +134,9 @@ export const CreateView = () => {
                         </Tab.Panel>
                         <Tab.Panel>
                         <div className="grid grid-cols-1 gap-6 items-start">
+                            <NFTDisplay 
+                                nft={nft}
+                            />
                             <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-3 items-start">
                                 <Input
                                     title="Send Details"
@@ -116,7 +153,7 @@ export const CreateView = () => {
                                     placeholder="NFT Token ID"
                                     value={trade.gives.tokenId || ''}
                                     onChange={({ currentTarget }) =>
-                                        updateTrade('gives.amount', currentTarget.value)
+                                        updateTrade('gives.tokenId', currentTarget.value)
                                     }
                                     type="number"
                                     token={trade.gives.tokenId || true}
@@ -166,8 +203,17 @@ export const CreateView = () => {
                         </Button>
                     </Card>
                 </div>
-
-                <div className="flex flex-col">
+                
+                <Transition
+                    show={openTrades.size !== 0}
+                    enter="transition-opacity duration-75"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="transition-opacity duration-150"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                    className="flex flex-col"
+                >     
                     <h2 className="view-header">Open Trades</h2>
                     <Card>
                         <DataTable 
@@ -179,27 +225,9 @@ export const CreateView = () => {
                                 receiving: convertAmount('readable', (entry[1].gets.amount || ''), entry[1].gets.token),
                             }))}
                             type="manage"
+                            toolbar={false}
                         />
-                    </Card>
-                </div>
-
-                <Transition
-                    show={!!order.orderHash && !!order.multiAddr}
-                    enter="transition-opacity duration-75"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="transition-opacity duration-150"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                    className="flex flex-col justify-center items-center text-center"
-                >
-                    <p className="text-sm">Trade Link:</p>
-                    <CopyClipboard
-                        value={createTradeLink()}
-                        icon
-                        lg
-                        truncate={5}
-                    />
+                    </Card>               
                 </Transition>
             </div>
 
