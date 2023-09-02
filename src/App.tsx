43b290e9e5a56ca61662568821aa7a4f3abc7271
alo <0xyoyobojo@gmail.com>
2023-09-02 18:53:16 -0400

    added swap to nav items

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,6 @@ import { useSigner } from 'wagmi';
 import 'react-toastify/dist/ReactToastify.css';
 import { cryptoFromSeed } from '@pintswap/sdk/lib/p2p';
 import { SwapView } from './views/swap';
-import { useEffect } from 'react';
 
 setFallbackWETH('0x7a2088a1bFc9d81c55368AE168C2C02570cB814F');
 
@@ -64,7 +63,7 @@ function App() {
                     <Route path="/fulfill/:multiaddr/:hash" element={<FulfillView />} />
                     <Route path="/fulfill/:multiaddr/nft/:hash" element={<FulfillNFTView />} />
 
-                    <Route path="*" element={<Navigate to="/markets" />} />
+                    <Route path="*" element={<Navigate to="/swap" />} />
                 </Routes>
             </Base>
 
