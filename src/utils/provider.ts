a199fed9af5ed95c341976ec632e0cda9a3234bf
alo <0xyoyobojo@gmail.com>
2023-10-25 12:55:30 -0400

    back to v6

--- src/utils/provider.ts
+++ src/utils/provider.ts
@@ -1,24 +1,23 @@
-import { ethers, Signer, JsonRpcProvider } from 'ethers6';
-import { providers } from 'ethers';
+import { ethers, Signer, JsonRpcProvider, InfuraProvider } from 'ethers6';
 import { DEFAULT_CHAINID } from './constants';
 
 const INFURA_PROJECT_ID = process.env.REACT_APP_INFURA_KEY || '';
 const LLAMA_NODES_KEY = process.env.REACT_APP_LLAMA_NODES_KEY || '';
 
-export function providerFromChainId(chainId: number | string): any {
+export function providerFromChainId(chainId: number | string) {
     switch (Number(chainId)) {
         case 1:
-            return new providers.JsonRpcProvider(`https://eth.llamarpc.com/rpc/${LLAMA_NODES_KEY}`);
+            return new JsonRpcProvider(`https://eth.llamarpc.com/rpc/${LLAMA_NODES_KEY}`);
         case 137:
-            return new providers.InfuraProvider('polygon', INFURA_PROJECT_ID);
+            return new InfuraProvider('polygon', INFURA_PROJECT_ID);
         case 42161:
-            return new providers.InfuraProvider('arbitrum', INFURA_PROJECT_ID);
+            return new InfuraProvider('arbitrum', INFURA_PROJECT_ID);
         case 43114:
-            return new providers.JsonRpcProvider('https://api.avax.network/ext/bc/C/rpc');
+            return new JsonRpcProvider('https://api.avax.network/ext/bc/C/rpc');
         case 10:
-            return new providers.JsonRpcProvider('https://mainnet.optimism.io');
+            return new JsonRpcProvider('https://mainnet.optimism.io');
         default:
-            return new providers.InfuraProvider('mainnet', INFURA_PROJECT_ID);
+            return new InfuraProvider('mainnet', INFURA_PROJECT_ID);
     }
 }
 
