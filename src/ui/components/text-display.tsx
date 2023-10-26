640120c5cdb32994af59cb2c2d0f25625850c30e
alo <0xyoyobojo@gmail.com>
2023-10-26 13:54:27 -0400

    better loading on nft trade

--- src/ui/components/text-display.tsx
+++ src/ui/components/text-display.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 import { SmartPrice } from './smart-price';
+import { Skeleton } from './skeleton';
 
 type ITextDisplayProps = {
     label?: string;
@@ -7,6 +8,7 @@ type ITextDisplayProps = {
     size?: `text-${string}`;
     usdValue?: string | number;
     align?: 'left' | 'right';
+    loading?: boolean | undefined;
 };
 
 export const TextDisplay = ({
@@ -15,24 +17,27 @@ export const TextDisplay = ({
     size,
     usdValue,
     align = 'left',
+    loading,
 }: ITextDisplayProps) => {
     const determineAlign = () => {};
     return (
         <div className={`flex flex-col ${align === 'right' ? 'justify-end text-right' : ''}`}>
             <span className="text-xs text-neutral-500">{label}</span>
-            <span
-                className={`flex items-center gap-3 ${
-                    align === 'right' ? 'justify-end text-right' : ''
-                }`}
-            >
-                <span className={`${size || 'text-lg'} `}>{value}</span>
-                {usdValue && (
-                    <span className="flex items-center text-neutral-300">
-                        (<span className="text-xs">$</span>
-                        <SmartPrice price={usdValue} />)
-                    </span>
-                )}
-            </span>
+            <Skeleton loading={loading} rounding="rounded" innerClass="!px-0">
+                <span
+                    className={`flex items-center gap-3 ${
+                        align === 'right' ? 'justify-end text-right' : ''
+                    }`}
+                >
+                    <span className={`${size || 'text-lg'} `}>{value}</span>
+                    {usdValue && (
+                        <span className="flex items-center text-neutral-300">
+                            (<span className="text-xs">$</span>
+                            <SmartPrice price={usdValue} />)
+                        </span>
+                    )}
+                </span>
+            </Skeleton>
         </div>
     );
 };
