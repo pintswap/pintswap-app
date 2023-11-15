652153b5d1842cc56f4056629a689171aaf607d3
alo <0xyoyobojo@gmail.com>
2023-11-15 16:22:04 -0500

    changed spinner all across

--- src/ui/components/spinner-loader.tsx
+++ src/ui/components/spinner-loader.tsx
@@ -1,19 +1,18 @@
-import { ImSpinner9 } from 'react-icons/im';
-
 type ISpinnerLoaderProps = {
-    color?: `text-${string}`;
     size?: `${string}px`;
     height?: `min-h-${string}`;
     className?: string;
 };
 
 export const SpinnerLoader = ({
-    color = 'text-gray-400',
     size = '20px',
     height = 'min-h-[50px]',
     className,
 }: ISpinnerLoaderProps) => (
     <div className={`flex ${className ? className : ''} justify-center items-center ${height}`}>
-        <ImSpinner9 className={`animate-spin ${color}`} size={size} />
+        <div
+            className="Toastify__spinner"
+            style={{ height: size || 'auto', width: size || 'auto' }}
+        />
     </div>
 );
