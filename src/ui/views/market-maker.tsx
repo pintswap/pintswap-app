201338330e136d26c84c2b84a4c358bf520209cd
Ian <ianhaley0@gmail.com>
2023-11-16 16:15:50 -0500

    position fix

--- src/ui/views/market-maker.tsx
+++ src/ui/views/market-maker.tsx
@@ -220,7 +220,7 @@ export default function MarketMakerView() {
                 <h2 className="view-header text-left">Market Maker</h2>
                 <Card className="!p-4 text-center">
                     <div className="flex justify-center shadow-inner items-center">
-                        <div className="p-4 pr-16 max-[400]:bg-green flex flex-col justify-center text-center mt-2  bg-neutral-900 rounded-lg h-40 ">
+                        <div className="p-4 pr-16 max-[400]:bg-green flex flex-col justify-center text-center w-52 mt-2  bg-neutral-900 rounded-lg h-40 ">
                             <h3 className="basis-1/3 ">Quote Token</h3>
                             <div className="basis-1/3  flex flex-col justify-center">
                                 <SelectCoin
@@ -236,12 +236,12 @@ export default function MarketMakerView() {
 
                             <div className="basis-1/3 "></div>
                         </div>
-                        <div className="bg-brand-dashboard rounded-lg flex w-7 h-40 justify-center items-center">
+                        <div className="bg-brand-dashboard rounded-lg flex w-7 h-40 justify-center items-center relative">
                             <div className=" absolute bg-brand-dashboard rounded-lg">
                                 <TbSlash color={'white'} size={'6rem'} />
                             </div>
                         </div>
-                        <div className="p-4 pl-16 flex flex-col justify-center text-center mt-2  bg-neutral-900 rounded-lg h-40">
+                        <div className="p-4 pl-16 flex flex-col justify-center w-52 text-center mt-2  bg-neutral-900 rounded-lg h-40">
                             <h3 className="basis-1/3">Base Token</h3>
                             <div className="basis-1/3 flex flex-col justify-center">
                                 <SelectCoin
